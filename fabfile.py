46ef71a1ecb7d8d2c528fb4d4b1e3bcc8b66dc48
Aleksander Matusiak <matusiak.aleksander@gmail.com>
2016-10-07 17:01:37 +0200

    Update deploy files for 2 VMs and add parsing response time

--- fabfile.py
+++ fabfile.py
@@ -1,48 +1,74 @@
 
 from fabric.api import *
 from fabric.context_managers import settings
+import time
 
 env.use_ssh_config = True
 
-@hosts('asl11')
 def deploy():
 	local('git push')
-	run('cd asl-fall16-project')
-	run('git pull')
+	with settings(host_string='asl11'):
+		run('cd asl-fall16-project')
+		run('git pull')
 
-@hosts('asl11')
+def copy_parse():
+	for host in ['asl2', 'asl3']:
+		local('scp parse_logs.py %s:~' % host)	
+
 def run_middleware():
 	threads = 3
 	rep = 1
-	run('cd asl-fall16-project')
-	runbg('java -jar ./dist/middleware-matusiaa.jar -I 10.0.0.4 -p 11212 -t %d -r %d -m 10.0.0.5:11212' % (threads, rep))
+	with settings(host_string='asl11'):
+		with cd('asl-fall16-project'):
+			runbg('java -jar ./dist/middleware-matusiaa.jar -I 10.0.0.4 -p 11212 -t %d -r %d -m 10.0.0.5:11212' % (threads, rep))
 
 def run_memcached():
 	with settings(host_string='asl1'):
 		runbg('memcached -p 11212 -t 1')
 
-def run_memaslap(time, clients):
+def run_memaslap(run_time, clients):
 	with settings(host_string='asl2'):
 		with cd('libmemcached-1.0.18'):
-			run('./clients/memaslap -s 10.0.0.12:11212 -T %d -c %d -o1 -S %s -t %s > ../logs/microbench%d' 
-					% (clients, clients, time, time, clients))
+			run('./clients/memaslap -s 10.0.0.12:11212 -T %d -c %d -o 0.9 -S %s -t %s > ../logs/microbench%d' 
+					% (clients, clients, run_time, run_time, clients))
 
-@hosts('asl11')
+def run_memaslap_async(host, run_time, clients, series_number):
+	with settings(host_string=host):
+		with cd('libmemcached-1.0.18'):
+			runbg('./clients/memaslap -s 10.0.0.12:11212 -T %d -c %d -o 0.9 -S %ss -t %ss' 
+					% (clients, clients, run_time, run_time),
+					'../logs/microbench%d_%d' % (clients, series_number))
+
 def stop_middleware():
-	run('pkill -f middleware')
+	with settings(host_string='asl11'):
+		run('pkill -f middleware')
 
 def stop_memcached():
 	with settings(host_string='asl1'):
 		run('pkill -f memcached')
 
+def runbg(cmd, output_file=None, sockname="dtach"):
+	if output_file:
+		cmd = "/bin/bash -c '{} > {}'".format(cmd, output_file)
+	else:
+		cmd = "/bin/bash -c '{}'".format(cmd)
+	return run('dtach -n `mktemp -u /tmp/%s.XXXX` %s'  % (sockname,cmd))
 
-def runbg(cmd, sockname="dtach"):
-    return run('dtach -n `mktemp -u /tmp/%s.XXXX` %s'  % (sockname,cmd))
-
-def run_experiment():
-	time = '30s'
+def run_experiments():
+	run_time = '30s'
 	max_clients = 32
 	run_memcached()
 	for clients in range(1, max_clients + 1):
-		run_memaslap(time, clients)
+		run_memaslap(run_time, clients)
+	stop_memcached()
+
+def run_double_experiments(series_number = 1):
+	run_time = 30
+	pause = 5
+	max_clients = 64
+	run_memcached()
+	for clients in range(1, max_clients + 1):
+		run_memaslap_async('asl2', run_time, clients, series_number)
+		run_memaslap_async('asl3', run_time, clients, series_number)
+		time.sleep(run_time + pause)
 	stop_memcached()
--- parse_logs.py
+++ parse_logs.py
@@ -1,7 +1,8 @@
-def parse(clients):
+def parse_throughput(clients):
 	fbase = 'logs/microbench'
+	series_number = 1
 	for i in range(1, clients + 1):
-		fname = fbase + str(i) 
+		fname = fbase + str(i) + '_' + str(series_number) 
 		with open(fname, 'r') as fh:
 			for line in fh:
 			    pass
@@ -10,3 +11,21 @@ def parse(clients):
 			start = last.find(pat) + len(pat)
 			end = line[start:].find(' ')
 			print(line[start:start+end])
+
+def parse_response_time(clients):
+	fbase = 'logs/microbench'
+	for i in range(1, clients + 1):
+		fname = fbase + str(i) 
+		with open(fname, 'r') as fh:
+			lines = fh.readlines()
+			for i in range(0, len(lines)):
+				line = lines[i]
+				if line.find('Total Statistics (') != -1:
+					i += 3
+					line = lines[i]
+					line = line.replace('Avg:', '')
+					line = line.replace(' ', ''); 
+					line = line.replace('\n', ''); 
+					print(line)
+					break
+				i += 1
